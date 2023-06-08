f932f0debf255bb8eef77d3c94f32d06c46535fd
simonescoca <simone.scoca@icloud.com>
2023-06-08 12:28:15 +0200

    added a func that picks chat active index

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
     // * Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
 
 // # Milestone 2
-    // * Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+    // * Visualizzazione dinamica dei messaggi: tramite la direttiva v-for:
+        // * visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
     // * Click sul contatto mostra la conversazione del contatto cliccato
 
 // # Milestone 3
@@ -11,7 +12,8 @@
     // * Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 
 // # Milestone 4
-    // * Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+    // * Ricerca utenti: scrivendo qualcosa nell’input a sinistra,
+    // * vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
 
 // # Milestone 5 - opzionale
     // * Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
@@ -236,11 +238,14 @@ createApp ({
                         }
                     ],
                 }
-            ]
+            ],
+            activeIndex: 0
         }
     },
 
     methods: {
-        
+        getActiveIndex (chatIndex) {
+            this.activeIndex = chatIndex
+        }
     }
 }).mount ("#app")
